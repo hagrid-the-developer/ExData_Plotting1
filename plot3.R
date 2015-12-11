f8b5b90e289cd93c22274d83ef39b323f099b506
Daniel Fiala <freelancer@meave.biz>
2015-12-11 18:47:36 +0100

    plot3 does load only necessary data.

--- plot3.R
+++ plot3.R
@@ -1,12 +1,9 @@
 
 gen.plot3 <- function(file) {
-	col.classes <- c('character', 'character', 'numeric', 'numeric', 'numeric', 'NULL', 'numeric', 'numeric', 'numeric')
+	col.classes <- c('character', 'character', 'NULL', 'NULL', 'NULL', 'NULL', 'numeric', 'numeric', 'numeric')
 	data <- read.csv(file, header=TRUE, sep=';', na.strings='?', quote='', colClasses=col.classes)
 	data <- data[ data$Date %in% c('1/2/2007', '2/2/2007'), ]
 	data <- data.frame(date=strptime(paste(data$Date, data$Time), format='%d/%m/%Y %H:%M:%S'),
-			   Global_active_power=data$Global_active_power,
-			   Voltage=data$Voltage,
-			   Global_reactive_power=data$Global_reactive_power,
 			   Sub_metering_1=data$Sub_metering_1,
 			   Sub_metering_2=data$Sub_metering_2,
 			   Sub_metering_3=data$Sub_metering_3)
--- plot4.R
+++ plot4.R
@@ -16,7 +16,7 @@ gen.plot4 <- function(file) {
 	lines(data$date, data$Sub_metering_2, type='l', col='red')
 	lines(data$date, data$Sub_metering_3, type='l', col='blue')
 	title(ylab='Energy sub metering')
-	legend('topright', lty = 1, col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'))
+	legend('topright', lty = 1, bty = 'n', cex = 2.0, col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'))
 
 	data
 }
